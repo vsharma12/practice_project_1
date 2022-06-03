ba2730ee35e4e792f1a03b8451f428f1adfb2054
varun <sharmavarun2504@gmail.com>
2022-06-03 22:45:34 +0530

    second

--- js/main.js
+++ js/main.js
@@ -57,14 +57,30 @@ function exit() {
 
 // -----------------  Making carousel image ------------------------------\\
 let buttons = document.querySelectorAll('[data-carousel-button]')
-
+let data_set;
 buttons.forEach((element) => {
     element.addEventListener('click',(e)=>{
-        const data_set = element.getAttribute('data-carousel-button') === 'next' ? 1 : -1 ;
-        const slides = element.closest('[data-parent]').querySelector('[data-image]');
-        console.log(slides)
+        // const slides = element.closest('[data-parent]').querySelector('[data-image]');
+        // const attri = ele.parentElement.getAttribute('class');
+        // let images = document.getElementsByClassName(attri);
+        // let imge = Array.from(images);
+        // console.log(imge);
+        data_set = element.getAttribute('data-carousel-button') === 'next' ? 1 : -1 ;
+        let images = document.querySelectorAll('.img img');//trying to get the image in which i want to add the feature of carousel then i will loop over them and ill get the index of the image that is click using the variable ele which the image that is click then ill change it using a change function that ill update the image shown to the user
+        let image = Array.from(images); // this is the array of images..
+        let indexOfImage = image.indexOf(ele); // This is the index of the image that is shown when you clicked an image
+        let newIndex = indexOfImage + data_set; //new index of the image, old index of the image adds the data_set value according to which button is clicked
+        if(newIndex < 0) newItndex =  image.length - 1;
+        if(newIndex >= image.length) newIndex = 0;
+        // now i have to make a function to change the image accordin to the new index variable to populate change in the image shown to the user 
+        let newImage = image[newIndex]
+        updateImage(newImage);
     });
 });
+
+function updateImage(Image) {
+    document.querySelector('.popup-image img').src = Image.getAttribute('src');
+}
 
 
 
